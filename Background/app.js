3ad25bb8f32fae4cf939bc00f6dc6f56cdecb8bb
Luke1836 <codingluke2023@gmail.com>
2024-02-20 13:49:24 +0530

    Particles added

--- Background/app.js
+++ Background/app.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const particlesArray = [];
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
@@ -11,5 +10,37 @@ window.addEventListener('resize', () => {
 
 class Particles 
 {
-    
+    constructor(effect)
+    {
+        this.effect = effect;
+        this.x = Math.random() * this.effect.width;
+        this.y = Math.random() * this.effect.height;
+        this.radius = 15;
+    }
+
+    draw(context)
+    {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fill();
+    }
+}
+
+class Effects 
+{
+    constructor(canvas)
+    {
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.particles = [];
+        this.numberOfParticles = 30;
+        this.createParticles();
+    }
+
+    createParticles()
+    {
+        for(let i = 0 ; i < this.numberOfParticles ; i++)
+            this.particles.push(new Particles(this));
+    }
 }
