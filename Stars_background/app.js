828ae595c8dc5a05e4648197a3f55a90a01bf451
Luke1836 <codingluke2023@gmail.com>
2024-02-22 20:32:54 +0530

    Canvas styles and functionality added

---
+++ Stars_background/app.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
